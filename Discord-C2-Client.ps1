5eb375a3134963d93ad23f2ea25ed6b661ef319a
egieb <93350544+beigeworm@users.noreply.github.com>
2024-07-11 17:30:30 +0000

    Update Discord-C2-Client.ps1

--- Discord-C2-Client.ps1
+++ Discord-C2-Client.ps1
@@ -1,11 +1,7 @@
 
 # =====================================================================================================================================================
 <#
-    ExtraInfo: Get a list of further info and command examples
-
-    Pause: Pause the current authenticated session
-
-    Close: Close this session
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
 
 #>
 # =====================================================================================================================================================

